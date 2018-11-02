130dd0d15d080c25c96114e10893548e8eac55ad
Gabriel <gabriel.gurgel@greenwoodcollege.com>
2018-11-02 13:44:15 -0400

    added test comment

--- functionsintro.py
+++ functionsintro.py
@@ -1,14 +1,13 @@
-def oss(x, y):
-    product = x * y
-    return product
-print(oss(9, 10))
-#test
-
-
-def compare(m, n):
-    if m == n:
-        print("true")
+def large_num(a, b, c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
     else:
-        print("false")
-print(compare(6, 4))
+        return c
 
+print(large_num(int(input("num1")), int(input("num2")), int(input("num3"))))
+
+
+def sumdice(dice, numbrolls):
+    value = dice*numbrolls
--- october30.py
+++ october30.py
@@ -1,5 +1,26 @@
-def hello_print(first_half):
-    second_half = "world"
-    return first_half+second_half
+def doubleevan():
+    nomad = int(input("press a number"))
+    if nomad % 2 == 1:
+        return "-1"
+    elif nomad % 2 == 0:
+        return (nomad*2)
+
+print(doubleevan())
 
 
+def grade_percentage():
+    grade = int(input("your percentage"))
+    if grade <= 50:
+        return "f"
+    if 60 > grade >= 50:
+        return "e"
+    if 60 < grade <= 70:
+        return "d"
+    if 70 < grade <= 80:
+        return "c"
+    if 80 < grade < 90:
+        return "b"
+    if grade >= 90:
+        return "a"
+
+print(grade_percentage())
